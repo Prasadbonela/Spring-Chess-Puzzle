b3b8f31d5051bb55a88366c41c4c7563b086a98f
Prasad52892 <prasadbonela52892@gmail.com>
2024-09-12 16:26:31 +0530

    Solved

--- src/main/java/com/epam/rd/autotasks/chesspuzzles/ChessBoard.java
+++ src/main/java/com/epam/rd/autotasks/chesspuzzles/ChessBoard.java
@@ -3,8 +3,9 @@ package com.epam.rd.autotasks.chesspuzzles;
 import java.util.Collection;
 
 public interface ChessBoard {
-    static ChessBoard of(Collection<ChessPiece> pieces){
-        throw new UnsupportedOperationException();
+    static ChessBoard of(Collection<ChessPiece> pieces) {
+        // Implement a concrete method to return an instance of ChessBoardImpl
+        return new ChessBoardImpl(pieces);
     }
 
     String state();
